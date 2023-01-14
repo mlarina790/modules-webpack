55ff986c03b85f66fd51b8248e2e092be507322e
Marina <mlarina790@gmail.com>
2023-01-14 13:23:30 +0300

    files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    module: {
+       rules: [
+        {
+             test: /\.css$/,
+             use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }
+       ]
+    },
+    plugins: [
+         new HtmlWebpackPlugin({
+             template: './src/index.html'
+            }),
+        new MiniCssExtractPlugin()
+    ]
+}
